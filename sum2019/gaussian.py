1e4e87ab055808283bcb46d767a9f0be2110d4f9
Pierre-Henri Wuillemin <pierre-henri.wuillemin@lip6.fr>
2019-06-11 18:51:55 +0200

    creating folder for sum2019

---
+++ sum2019/gaussian.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import pyAgrum as gum
+import openturns as ot
+
+import otagrum as otagr
+
+R=ot.CorrelationMatrix(5)
+for i in range(5):
+    for j in range(i):
+        R[i,j]=0.8
+distribution = ot.Normal([0] * 5, [1] * 5, R)
+#help(distribution)
+print(distribution)
+
+
+dag = gum.DAG()
+for i in range(5):
+  dag.addNode()
+dag.addArc(0, 1)
+print(dag)
