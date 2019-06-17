612a6e96469d967bd563edb7b52b342e072d3e26
Marvin Lasserre <marvin.lasserre@lip6.fr>
2019-06-17 19:26:24 +0200

    Compute mean log_likelihood with 10-fold CV

--- sum2019/wine_quality.py
+++ sum2019/wine_quality.py
@@ -2,16 +2,36 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from sklearn.model_selection import KFold
+
 import openturns as ot
+
+import score as sc
 import hill_climbing as hc
 
-ot.RandomGenerator.SetSeed(42)
-np.random.seed(42)
+
+#ot.RandomGenerator.SetSeed(42)
+#np.random.seed(42)
 
 red_wine = ot.Sample.ImportFromCSVFile("data/winequality/winequality-red.csv")
 red_wine = (red_wine.rank()+1)/(red_wine.getSize()+2)
+red_wine = np.array(red_wine)
 
-G, S = hc.hill_climbing(red_wine,4,2)
+kf = KFold(n_splits=10, shuffle=True)
 
-print("G: ", G)
-print("S: ", S)
+C, G, S, L = [], [], [], []
+for max_parents in range(5):
+    print("Loop: ", max_parents)
+    lc, lg, ls, ll = [], [], [], []
+    for train_index, test_index in kf.split(red_wine):
+        train, test = red_wine[train_index], red_wine[test_index]
+        c, g, s = hc.hill_climbing(ot.Sample(train), max_parents, 5)
+        
+        lc.append(c)
+        lg.append(g)
+        ls.append(s)
+        ll.append(sc.log_likelihood(ot.Sample(test), c, g)/test.shape[0])
+    C.append(lc)
+    G.append(lg)
+    S.append(ls)
+    L.append(ll)
