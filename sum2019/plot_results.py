39becc32fca60258d3283a4ef78ec52174e816fa
Marvin Lasserre <marvin.lasserre@lip6.fr>
2019-06-20 17:28:11 +0200

    Adding error bars

--- sum2019/plot_results.py
+++ sum2019/plot_results.py
@@ -11,7 +11,7 @@ mpl.rc('text', usetex=True)
 mpl.rc('font', family='serif')
 
 fig_path = "fig"
-data_set_path = "data/winequality/elidan_winequality-red_k10r1.csv"
+data_set_path = "data/winequality/elidan_winequality-red_k10r1mp10.csv"
 data_set_name = data_set_path.split('/')[-1].split('.')[0]
 
 data = np.loadtxt(data_set_path, delimiter=',').transpose()
@@ -31,5 +31,10 @@ ax.set_ylabel('')
 #plt.ylabel(nameOfPlot,rotation=0)
 #ax.legend(frameon=False, loc='upper left',ncol=2,handlelength=4)
 
+upper = data[1] - data[2]
+lower = data[1] + data[2]
+
 ax.plot(data[0].astype(int), data[1])
+
+ax.fill_between(data[0], lower, upper, alpha=0.4)
 plt.savefig(os.path.join(fig_path, data_set_name+".pdf"), transparent=True)
