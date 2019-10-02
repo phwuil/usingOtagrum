b5073b5265ddcc0f3eeb81b026636e91f614e516
Marvin Lasserre <marvin.lasserre@lip6.fr>
2019-10-02 20:14:06 +0200

    Compute likelihood for cpc and elidan

--- flairs2020/src/elidan_learning.py
+++ flairs2020/src/elidan_learning.py
@@ -9,20 +9,21 @@ import openturns as ot
 import score as sc
 import hill_climbing as hc
 
+import os.path as path
 
 #ot.RandomGenerator.SetSeed(42)
 #np.random.seed(42)
 
-data_set_path = "data/winequality/winequality-red.csv"
+data_set_path = "../data/samples/winequality/winequality-red.csv"
 data_set_name = data_set_path.split('/')[-1].split('.')[0]
 
-red_wine = ot.Sample.ImportFromCSVFile(data_set_path)
-red_wine = (red_wine.rank()+1)/(red_wine.getSize()+2)
-red_wine = np.array(red_wine)
+data = ot.Sample.ImportFromTextFile(data_set_path, ';')
+data = (data.rank()+1)/(data.getSize()+2)
+data = np.array(data)
 
-k = 10
-n_restart = 1
-stop_max_parents = 10
+k = 10                 # Number of fold for cross-validation
+n_restart = 1          # Number of restart
+stop_max_parents = 5  # Maximum number of maximum parents
 
 
 kf = KFold(n_splits=k, shuffle=True)
@@ -31,8 +32,8 @@ Copulas, Graphs, Scores, Loglikelihoods = [], [], [], []
 for max_parents in range(stop_max_parents+1):
     print("Loop: ", max_parents)
     list_copulas, list_graphs, list_scores, list_loglikelihoods = [], [], [], []
-    for train_index, test_index in kf.split(red_wine):
-        train, test = red_wine[train_index], red_wine[test_index]
+    for train_index, test_index in kf.split(data):
+        train, test = data[train_index], data[test_index]
         c, g, s = hc.hill_climbing(ot.Sample(train), max_parents, n_restart)
         
         list_copulas.append(c)
@@ -55,4 +56,4 @@ header = "k=" + str(k) + ", " + "restarts=" + str(n_restart)
 title = "elidan_"  + data_set_name + "_k" + str(k) + "r" + str(n_restart) + \
         "mp" + str(stop_max_parents) + ".csv"
 
-np.savetxt(title, results, fmt="%f", delimiter=',', header=header)
+np.savetxt(path.join("../results/winequality/",title), results, fmt="%f", delimiter=',', header=header)
--- flairs2020/src/elidan_performances.py
+++ flairs2020/src/elidan_performances.py
@@ -13,74 +13,50 @@ import otagrum as otagr
 import score as sc
 import hill_climbing as hc
 
-computeScores = False
-computeLogLikelihood = True
 
 max_parents = 4
 n_restart = 2
 k = 10
-n_samples = 50
+n_samples = 5
 
-data_set_path = "data/sample_regis/sample.csv"
-
+data_set_path = "../data/samples/gaussian/asia/r08/asia_gaussian_sample_01.csv"
 data_set_name = data_set_path.split('/')[-1].split('.')[0]
-
 data = np.loadtxt(data_set_path, delimiter=',', skiprows=1)
 
-if computeScores:
-    
-    Tstruct_file_path = "data/cbn2/struct.txt"
-    Tstruct_file_name = Tstruct_file_path.split('/')[-1].split('.')[0]
-    
-    with open(Tstruct_file_path, 'r') as file:
-        arcs = file.read().replace('\n', '')
-    Tstruct = gum.fastBN(arcs)    
-    
-    data = ot.Sample(data)
-    c, g, s = hc.hill_climbing(ot.Sample(data), max_parents, n_restart)
-    
-    bn = gum.BayesNet()
-    for name in Tstruct.names():
-        bn.add(gum.LabelizedVariable(name))
-    for arc in g.arcs():
-        bn.addArc(arc[0], arc[1])
-    
-    comparison = GraphicalBNComparator(Tstruct, bn)
-    print(comparison.scores())
 
-
-if computeLogLikelihood:
-    sizes = np.linspace(20, 500, n_samples, dtype=int)
-    Loglikelihoods = []
-    Structures = []
-    for size in sizes:
-        print(size)
-        sample = data[np.random.randint(0,len(data), size=size)]
+Loglikelihoods = []
+Structures = []
+sizes = np.linspace(100, 500, n_samples, dtype=int)
+for size in sizes:
+    print(size)
+    sample = data[np.random.choice(np.arange(0, len(data)),
+                                       size=size,
+                                       replace=False)]
+    
+    kf = KFold(n_splits=k, shuffle=True)
+    
+    list_loglikelihoods = []
+    list_structures = []
+    for train_index, test_index in kf.split(sample):
+        train, test = sample[train_index], sample[test_index]
+        c, g, s = hc.hill_climbing(ot.Sample(train), max_parents, n_restart)
+        list_loglikelihoods.append(sc.log_likelihood(ot.Sample(test), c, g)/test.shape[0])
+        list_structures.append(g)
         
-        kf = KFold(n_splits=k, shuffle=True)
-        
-        list_loglikelihoods = []
-        list_structures = []
-        for train_index, test_index in kf.split(sample):
-            train, test = sample[train_index], sample[test_index]
-            c, g, s = hc.hill_climbing(ot.Sample(train), max_parents, n_restart)
-            list_loglikelihoods.append(sc.log_likelihood(ot.Sample(test), c, g)/test.shape[0])
-            list_structures.append(g)
-            
-        Loglikelihoods.append(list_loglikelihoods)
-        Structures.append(list_structures)
-    
-    Loglikelihoods = np.array(Loglikelihoods, dtype=float)
-    ll_mean = np.mean(Loglikelihoods, axis=1, keepdims=True)
-    ll_std = Loglikelihoods.std(axis=1, keepdims=True)
-    
-    ll_mean = ll_mean.reshape(n_samples, 1)
-    ll_std = ll_std.reshape(n_samples, 1)
-    sizes = sizes.reshape(n_samples,1)
-    results = np.concatenate((sizes, ll_mean, ll_std), axis=1)
-    
-    header = "k=" + str(k) + ", " + "restarts=" + str(n_restart)
-    title = "elidan_"  + data_set_name + "_k" + str(k) + "r" + str(n_restart) + \
-            "mp" + str(max_parents) + "s" + str(n_samples) + ".csv"
+    Loglikelihoods.append(list_loglikelihoods)
+    Structures.append(list_structures)
 
-    np.savetxt(title, results, fmt="%f", delimiter=',', header=header)
+Loglikelihoods = np.array(Loglikelihoods, dtype=float)
+ll_mean = np.mean(Loglikelihoods, axis=1, keepdims=True)
+ll_std = Loglikelihoods.std(axis=1, keepdims=True)
+
+ll_mean = ll_mean.reshape(n_samples, 1)
+ll_std = ll_std.reshape(n_samples, 1)
+sizes = sizes.reshape(n_samples,1)
+results = np.concatenate((sizes, ll_mean, ll_std), axis=1)
+
+header = "k=" + str(k) + ", " + "restarts=" + str(n_restart)
+title = "elidan_"  + data_set_name + "_k" + str(k) + "r" + str(n_restart) + \
+        "mp" + str(max_parents) + "s" + str(n_samples) + ".csv"
+
+np.savetxt(title, results, fmt="%f", delimiter=',', header=header)
