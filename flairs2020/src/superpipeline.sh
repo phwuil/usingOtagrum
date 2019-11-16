5a7497f5f15004ed0a9557ddca5372645c211bad
Marvin Lasserre <marvin.lasserre@lip6.fr>
2019-11-16 12:51:18 +0100

    Added figure to plot

--- flairs2020/src/superpipeline.sh
+++ flairs2020/src/superpipeline.sh
@@ -1,4 +1,14 @@
-sh pipeline.sh -D gaussian -C 0.8 -S asia -M cpc --compute scores --recompute --replot --score dag;
+#sh pipeline.sh -D gaussian -C 0.8 -S asia -M cpc --compute scores --replot --score skeleton;
+
+sh pipeline.sh -D gaussian -C 0.8 -S alarm -M cpc --compute scores --replot --score all;
+sh pipeline.sh -D student -C 0.8 -S alarm -M cpc --compute scores --replot --score all;
+sh pipeline.sh -D dirichlet -C 0.8 -S alarm -M cpc --compute scores --replot --score all;
+
+#sh pipeline.sh -D gaussian -C 0.8 -S asia -M elidan --compute scores --replot --score all;
+#sh pipeline.sh -D student -C 0.8 -S asia -M elidan --compute scores --replot --score all;
+#sh pipeline.sh -D dirichlet -C 0.8 -S asia -M elidan --compute scores --replot --score all;
+#sh pipeline.sh -D gaussian -C 0.8 -S alarm -M elidan --compute scores --recompute --replot --score skeleton;
+#sh pipeline.sh -D gaussian -C 0.8 -S asia -M elidan --compute scores --replot --score skeleton;
 #sh pipeline.sh -D gaussian -C 0.8 -S asia -M elidan --compute scores --replot;
 
 #sh pipeline.sh -D dirichlet -C 0.8 -S asia -M cpc --compute scores --replot;
@@ -6,3 +16,5 @@ sh pipeline.sh -D gaussian -C 0.8 -S asia -M cpc --compute scores --recompute --
 
 #sh pipeline.sh -D student -C 0.8 -S asia -M cpc --compute scores --replot;
 #sh pipeline.sh -D student -C 0.8 -S asia -M elidan --compute scores --replot;
+
+#sh pipeline.sh -D gaussian -C 0.8 -S asia -M elidan --compute time --replot;
