e4da6bd3a82b940ff0676be5032fc21d194782a2
Marvin Lasserre <marvin.lasserre@lip6.fr>
2020-09-09 01:39:34 +0200

    Script generating random graphs

---
+++ comparing_algo/src/generate_graph.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# It would be nice to put this in the pipeline
+import pyAgrum as gum
+import otagrum as otagr
+import graph_utils as gu
+
+from pathlib import Path
+
+step = 10
+start_size = 2
+end_size = 102
+restart = 2
+
+density = 1.2
+
+# Setting directories location and files
+directory = Path("../data/structures/generated/")
+directory.mkdir(parents=True, exist_ok=True)
+
+generator = gum.BNGenerator()
+gum.initRandom(10)
+
+for r in range(restart):
+    for i in range(start_size, end_size+1, step):
+        print("Number of node :", i, flush=True)
+        file_name = "size_" + str(i).zfill(3) + "_" + str(r+1).zfill(2)
+
+        n_arc = int( density*(i-1) )
+        bn = generator.generate(i, n_arc)
+        ndag = otagr.NamedDAG(bn.dag(), bn.names())
+        gu.write_graph(ndag, directory.joinpath(file_name + '.dot'))
