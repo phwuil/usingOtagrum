d72a71ff371c4f3e9eafdab890be325ce99862ce
Marvin Lasserre <marvin.lasserre@lip6.fr>
2021-03-11 19:24:57 +0100

    Updates for new CBN API

--- comparing_algo/src/data_generation.py
+++ comparing_algo/src/data_generation.py
@@ -29,27 +29,27 @@ def generate_gaussian_copulas(ndag, r=0.8):
     return lcc
 
 def generate_student_data(ndag, size, r=0.8):
-    order=ndag.getTopologicalOrder()
-    jointDistributions=[]
+    order = ndag.getTopologicalOrder()
+    copulas = []
     for k in range(order.getSize()):
         d = 1 + ndag.getParents(k).getSize()
         R = ot.CorrelationMatrix(d)
         for i in range(d):
             for j in range(i):
                 R[i, j] = r
-        jointDistributions.append(ot.Student(5.0, [0.0]*d, [1.0]*d, R).getCopula())
-    copula = otagr.ContinuousBayesianNetwork(ndag, jointDistributions)
-    sample = copula.getSample(size)
+        copulas.append(ot.Student(5.0, [0.0]*d, [1.0]*d, R).getCopula())
+    cbn = otagr.ContinuousBayesianNetwork(ndag, [ot.Uniform(0., 1.)]*ndag.getSize(), copulas)
+    sample = cbn.getSample(size)
     return sample
 
 def generate_dirichlet_data(ndag, size):
-    order=ndag.getTopologicalOrder()
-    jointDistributions=[]
+    order = ndag.getTopologicalOrder()
+    copulas = []
     for k in range(order.getSize()):
         d = 1 + ndag.getParents(k).getSize()
-        jointDistributions.append(ot.Dirichlet([(1.0+k)/(d+1) for k in range(d+1)]).getCopula())
-    copula = otagr.ContinuousBayesianNetwork(ndag, jointDistributions)
-    sample = copula.getSample(size)
+        copulas.append(ot.Dirichlet([(1.0+k)/(d+1) for k in range(d+1)]).getCopula())
+    cbn = otagr.ContinuousBayesianNetwork(ndag, [ot.Uniform(0., 1.)]*ndag.getSize(), copulas)
+    sample = cbn.getSample(size)
     return sample
 
 def generate_data(ndag, size, distribution, **kwargs):
