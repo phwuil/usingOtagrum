af0d6dc24f43ad6a88d23ff855810276c5158117
Marvin Lasserre <marvin.lasserre@lip6.fr>
2021-09-30 17:03:22 +0200

    Changed test

--- comparing_algo/src/testOne.py
+++ comparing_algo/src/testOne.py
@@ -9,17 +9,18 @@ import numpy as np
 
 from pipeline import Pipeline
 import graph_utils as gu
-import elidan.hill_climbing as hc
+# import elidan.hill_climbing as hc
 
-data = ot.Sample.ImportFromTextFile("../data/samples/dirichlet/alarm/sample01.csv", ',')
+data = ot.Sample.ImportFromTextFile("../data/samples/gaussian/chain_vs/r08/sample01.csv", ',')
 
 # learner = cmiic.ContinuousMIIC(data,
                                # cmode=cmiic.CModeType.Bernstein,
                                # kmode=cmiic.KModeType.Naive)
-learner = otagrum.TabuList(data, 4, 5, 5)
+# learner = otagrum.TabuList(data, 4, 5, 5)
+learner = otagrum.ContinuousPC(data, 5, 0.05)
 learner.setVerbosity(True)
 dag = learner.learnDAG()
-gu.write_graph(dag, "output_tabulist_gaussian.dot")
+gu.write_graph(dag, "output_cpc_gaussian.dot")
 
 # learner = otagrum.TabuList(data, 4, 5, 5)
 # learner.setCMode(otagrum.CorrectedMutualInformation.CModeTypes_Bernstein)
@@ -27,10 +28,10 @@ gu.write_graph(dag, "output_tabulist_gaussian.dot")
 # dag = learner.learnDAG()
 # gu.write_graph(dag, "output_tabulist_bernstein.dot")
 
-t = hc.hill_climbing(data)
-names = list(data.getDescription())
-gu.write_graph(otagrum.NamedDAG(t[1], names), "output_hc.dot")
-print("Final score hc: ", t[2])
+# t = hc.hill_climbing(data)
+# names = list(data.getDescription())
+# gu.write_graph(otagrum.NamedDAG(t[1], names), "output_hc.dot")
+# print("Final score hc: ", t[2])
 # learner.use3off2()
 # pdag = learner.learnMixedStructure()
 # dag = learner.learnStructure()
